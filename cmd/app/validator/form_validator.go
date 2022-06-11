f51dfe8a9e063c5daa9924e75b63d004129a671b
Alvinditya Saputra <calvinjoe782@gmail.com>
2022-06-11 17:04:19 +0700

    update form validator

--- cmd/app/validator/form_validator.go
+++ cmd/app/validator/form_validator.go
@@ -3,6 +3,7 @@ package validator
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	respkey "learn-rest-api/cmd/app/constant"
 	"learn-rest-api/cmd/app/exception"
 	"strings"
@@ -10,6 +11,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
 
 func BindJSON(c *gin.Context, form any) {
 	var err = c.ShouldBindJSON(form)
@@ -27,14 +31,22 @@ func getErrorMsg(fe validator.FieldError) string {
         case "required":
             return fmt.Sprintf("`%s` is required", fe.Field())
         case "lte":
-            return fmt.Sprintf("Field `%s` must be less than or equal to %s", fe.Field(), fe.Param())
+            return fmt.Sprintf("Field `%s` must be less than or equal to %s", getFieldName(fe.Field()), fe.Param())
         case "gte":
-			return fmt.Sprintf("Field `%s` should be greater than %s", fe.Field(), fe.Param())
+			return fmt.Sprintf("Field `%s` should be greater than %s", getFieldName(fe.Field()), fe.Param())
 		case "min":
-			return fmt.Sprintf("Field `%s` should be at least %s characters", strings.ToLower(fe.Field()), fe.Param())
+			return fmt.Sprintf("Field `%s` should be at least %s characters", getFieldName(fe.Field()), fe.Param())
 		case "max":
-			return fmt.Sprintf("Field `%s` should be at most %s characters", strings.ToLower(fe.Field()), fe.Param())
+			return fmt.Sprintf("Field `%s` should be at most %s characters", getFieldName(fe.Field()), fe.Param())
+		case "len":
+			return fmt.Sprintf("Field `%s` must be %s characters", getFieldName(fe.Field()), fe.Param())
 		default:
-			return "Invalid field"
+			return "Invalid field validation"
     }
-}	
+}
+
+func getFieldName(value string) string {
+	value = matchFirstCap.ReplaceAllString(value, "${1}_${2}")
+    value  = matchAllCap.ReplaceAllString(value, "${1}_${2}")
+    return strings.ToLower(value)
+}
