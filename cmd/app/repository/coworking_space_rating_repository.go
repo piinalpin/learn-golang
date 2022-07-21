b017df8b05f378c41a737b316773af6b8593c9db
Alvinditya Saputra <alvindityas@gmail.com>
2022-07-21 20:58:51 +0700

    feat: refactor repository to throw exception

--- cmd/app/repository/coworking_space_rating_repository.go
+++ cmd/app/repository/coworking_space_rating_repository.go
@@ -1,18 +1,20 @@
 package repository
 
 import (
+	"learn-rest-api/cmd/app/constant"
 	"learn-rest-api/cmd/app/domain/dao"
+	"learn-rest-api/cmd/app/exception"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 type CoworkingSpaceRatingRepository interface {
-	FindAllCoworkingSpaceRating() ([]dao.CoworkingSpaceRating, error)
-	FindAllCoworkingSpaceRatingByCoworkingSpaceID(coworkingSpaceID int) ([]dao.CoworkingSpaceRating, error)
-	FindAllCoworkingSpaceRatingByUserID(userID int) ([]dao.CoworkingSpaceRating, error)
-	FindCoworkingSpaceRatingByID(id int) (dao.CoworkingSpaceRating, error)
-	SaveCoworkingSpaceRating(m *dao.CoworkingSpaceRating) (dao.CoworkingSpaceRating, error)
+	FindAllCoworkingSpaceRating() []dao.CoworkingSpaceRating
+	FindAllCoworkingSpaceRatingByCoworkingSpaceID(coworkingSpaceID int) []dao.CoworkingSpaceRating
+	FindAllCoworkingSpaceRatingByUserID(userID int) []dao.CoworkingSpaceRating
+	FindCoworkingSpaceRatingByID(id int) dao.CoworkingSpaceRating
+	SaveCoworkingSpaceRating(m *dao.CoworkingSpaceRating) dao.CoworkingSpaceRating
 }
 
 type coworkingSpaceRatingRepository struct {
@@ -25,65 +27,65 @@ func CoworkingSpaceRatingRepositoryInit(db *gorm.DB) CoworkingSpaceRatingReposit
 }
 
 // FindAllCoworkingSpaceRating implements CoworkingSpaceRatingRepository
-func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRating() ([]dao.CoworkingSpaceRating, error) {
+func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRating() []dao.CoworkingSpaceRating {
 	var coworkingSpaceRatings []dao.CoworkingSpaceRating
 	var err = cs.db.Find(&coworkingSpaceRatings).Error
 	if err != nil {
 		log.Error("Got an error when find all coworking space ratings. Error: ", err)
-		return nil, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
 	
-	return coworkingSpaceRatings, nil
+	return coworkingSpaceRatings
 }
 
 // FindAllCoworkingSpaceRatingByCoworkingSpaceID implements CoworkingSpaceRatingRepository
-func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRatingByCoworkingSpaceID(coworkingSpaceID int) ([]dao.CoworkingSpaceRating, error) {
+func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRatingByCoworkingSpaceID(coworkingSpaceID int) []dao.CoworkingSpaceRating {
 	var coworkingSpaceRatings []dao.CoworkingSpaceRating
 	var err = cs.db.Model(
 		dao.CoworkingSpaceRating {
-			CoworkingSpaceID: uint(coworkingSpaceID),
+			CoworkingSpaceID: coworkingSpaceID,
 		},
 	).Find(&coworkingSpaceRatings).Error
 	if err != nil {
 		log.Error("Got an error when find all coworking space ratings by coworking space id. Error: ", err)
-		return nil, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
-	return coworkingSpaceRatings, nil
+	return coworkingSpaceRatings
 }
 
 // FindAllCoworkingSpaceRatingByUserID implements CoworkingSpaceRatingRepository
-func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRatingByUserID(userID int) ([]dao.CoworkingSpaceRating, error) {
+func (cs *coworkingSpaceRatingRepository) FindAllCoworkingSpaceRatingByUserID(userID int) []dao.CoworkingSpaceRating {
 	var coworkingSpaceRatings []dao.CoworkingSpaceRating
 	var err = cs.db.Model(
 		dao.CoworkingSpaceRating {
-			UserID: uint(userID),
+			UserID: userID,
 		},
 	).Find(&coworkingSpaceRatings).Error
 	if err != nil {
 		log.Error("Got an error when find all coworking space ratings by user id. Error: ", err)
-		return nil, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
-	return coworkingSpaceRatings, nil
+	return coworkingSpaceRatings
 }
 
 // FindCoworkingSpaceRatingByID implements CoworkingSpaceRatingRepository
-func (cs *coworkingSpaceRatingRepository) FindCoworkingSpaceRatingByID(id int) (dao.CoworkingSpaceRating, error) {
+func (cs *coworkingSpaceRatingRepository) FindCoworkingSpaceRatingByID(id int) dao.CoworkingSpaceRating {
 	var coworkingSpaceRating dao.CoworkingSpaceRating
 	var err = cs.db.First(&coworkingSpaceRating, id).Error
 	if err != nil {
 		log.Error("Got an error when find coworking space rating by id. Error: ", err)
-		return dao.CoworkingSpaceRating{}, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
 
-	return coworkingSpaceRating, nil
+	return coworkingSpaceRating
 }
 
 // SaveCoworkingSpaceRating implements CoworkingSpaceRatingRepository
-func (cs *coworkingSpaceRatingRepository) SaveCoworkingSpaceRating(m *dao.CoworkingSpaceRating) (dao.CoworkingSpaceRating, error) {
+func (cs *coworkingSpaceRatingRepository) SaveCoworkingSpaceRating(m *dao.CoworkingSpaceRating) dao.CoworkingSpaceRating {
 	var err = cs.db.Save(m).Error
 	if err != nil {
 		log.Error("Got an error when save coworking space rating. Error: ", err)
-		return dao.CoworkingSpaceRating{}, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
-	return *m, nil
+	return *m
 }
--- cmd/app/repository/coworking_space_repository.go
+++ cmd/app/repository/coworking_space_repository.go
@@ -1,56 +1,68 @@
 package repository
 
 import (
+	"learn-rest-api/cmd/app/constant"
 	"learn-rest-api/cmd/app/domain/dao"
+	"learn-rest-api/cmd/app/exception"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
-type CoworkingSpaceRepostory interface {
-	FindCoworkingSpaceByID(id int) (dao.CoworkingSpace, error)
-	SaveCoworkingSpace(m *dao.CoworkingSpace) (dao.CoworkingSpace, error)
-	FindAllCoworkingSpace() ([]dao.CoworkingSpace, error)
+type CoworkingSpaceRepository interface {
+	FindCoworkingSpaceByID(id int) dao.CoworkingSpace
+	SaveCoworkingSpace(m *dao.CoworkingSpace) dao.CoworkingSpace
+	FindAllCoworkingSpace() []dao.CoworkingSpace
+	DeleteCoworkingSpace(m *dao.CoworkingSpace)
 }
 
 type coworkingSpaceRepository struct {
 	db *gorm.DB
 }
 
-func CoworkingSpaceRepositoryInit(db *gorm.DB) CoworkingSpaceRepostory {
+func CoworkingSpaceRepositoryInit(db *gorm.DB) CoworkingSpaceRepository {
 	db.AutoMigrate(&dao.CoworkingSpace{})
 	return &coworkingSpaceRepository{db: db}
 }
 
 // FindAllCoworkingSpace implements CoworkingSpaceRepostory
-func (cs *coworkingSpaceRepository) FindAllCoworkingSpace() ([]dao.CoworkingSpace, error) {
+func (cs *coworkingSpaceRepository) FindAllCoworkingSpace() []dao.CoworkingSpace {
 	var coworkingSpaces []dao.CoworkingSpace
 	var err = cs.db.Find(&coworkingSpaces).Error
 	if err != nil {
 		log.Error("Got an error when find all coworking spaces. Error: ", err)
-		return nil, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
 
-	return coworkingSpaces, nil
+	return coworkingSpaces
 }
 
 // FindCoworkingSpaceByID implements CoworkingSpaceRepostory
-func (cs *coworkingSpaceRepository) FindCoworkingSpaceByID(id int) (dao.CoworkingSpace, error) {
+func (cs *coworkingSpaceRepository) FindCoworkingSpaceByID(id int) dao.CoworkingSpace {
 	var coworkingSpace dao.CoworkingSpace
 	var err = cs.db.First(&coworkingSpace, id).Error
 	if err != nil {
 		log.Error("Got an error when find coworking space by id. Error: ", err)
-		return dao.CoworkingSpace{}, err
+		exception.ThrowNewAppException(constant.DataNotFound)
 	}
-	return coworkingSpace, nil
+	return coworkingSpace
 }
 
 // SaveCoworkingSpace implements CoworkingSpaceRepostory
-func (cs *coworkingSpaceRepository) SaveCoworkingSpace(m *dao.CoworkingSpace) (dao.CoworkingSpace, error) {
+func (cs *coworkingSpaceRepository) SaveCoworkingSpace(m *dao.CoworkingSpace) dao.CoworkingSpace {
 	var err = cs.db.Save(m).Error
 	if err != nil {
 		log.Error("Got an error when save coworking space. Error: ", err)
-		return dao.CoworkingSpace{}, err
+		exception.ThrowNewAppException(constant.UnknownError)
 	}
-	return *m, nil
+	return *m
+}
+
+// DeleteCoworkingSpace implements CoworkingSpaceRepostory
+func (cs *coworkingSpaceRepository) DeleteCoworkingSpace(m *dao.CoworkingSpace) {
+	var err = cs.db.Delete(m).Error
+	if err != nil {
+		log.Error("Got an error when delete coworking space. Error: ", err)
+		exception.ThrowNewAppException(constant.UnknownError)
+	}
 }
